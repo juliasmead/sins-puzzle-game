892e389e961d514f3e7e45d5b79f6db88f549398
Zilby <azilby@gmail.com>
2018-06-06 03:38:52 -0400

    added some utility scripts

---
+++ Assets/Scripts/Utility/AudioVarier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+/// <summary>
+/// This class varies the pitch for sounds so that
+/// they don't all overlap and drastically increase the amplitude.
+/// </summary>
+public class AudioVarier : MonoBehaviour {
+
+	/// <summary>
+	/// Audio source to be varied. 
+	/// </summary>
+	private AudioSource a;
+
+	void Start () {
+		a = GetComponent<AudioSource> ();
+		a.pitch += -0.1f + (Random.Range (0, 20000000) / 100000000.0f);
+	}
+}
