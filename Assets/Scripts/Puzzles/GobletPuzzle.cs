ebd8a74792b3a0a41a548b3f523bb0d7b49fb93d
Zilby <azilby@gmail.com>
2018-06-07 20:35:07 -0400

    goblet puzzle basic implementation done

---
+++ Assets/Scripts/Puzzles/GobletPuzzle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GobletPuzzle : MonoBehaviour {
+
+    /// <summary>
+    /// The list of sprites for each goblet. 
+    /// </summary>
+    public List<Sprite> gobletSprites;
+
+    public delegate Sprite GetSprite(Goblet.Type t);
+    /// <summary>
+    /// Gets the sprite for a given goblet. 
+    /// </summary>
+    public static GetSprite SpriteEvent;
+
+    private void Awake()
+    {
+        SpriteEvent = FindGobletSprite;
+    }
+
+    /// <summary>
+    /// Finds the sprite for the given goblet. 
+    /// </summary>
+    /// <returns>The goblet sprite.</returns>
+    private Sprite FindGobletSprite(Goblet.Type t)
+    {
+        if ((int)t < gobletSprites.Count && t != Goblet.Type.none)
+        {
+            return gobletSprites[(int)t];
+        }
+        return null;
+    }
+}
