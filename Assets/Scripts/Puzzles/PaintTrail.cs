9dffe38b136c1098b579be722d3efa7d57dd4bd6
Zilby <azilby@gmail.com>
2018-06-14 21:48:40 -0400

    PAINT HELL

---
+++ Assets/Scripts/Puzzles/PaintTrail.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintTrail : MonoBehaviour
+{
+    public enum ColorType
+    {
+        none,
+        redThin,
+        purpleTHICK,
+        blueTrans,
+    }
+
+    public ColorType color = ColorType.none;
+
+    public TrailRenderer trail;
+
+    public static int layer = 0;
+
+    public IEnumerator Paint() {
+        trail.sortingOrder = layer--;
+        switch(color) {
+            case ColorType.redThin:
+                trail.startColor = trail.endColor = Color.red;
+                trail.startWidth = trail.endWidth = 0.5f;
+                break;
+            case ColorType.purpleTHICK:
+                trail.startColor = trail.endColor = Color.magenta;
+                trail.startWidth = trail.endWidth = 1.5f;
+                break;
+            case ColorType.blueTrans:
+                trail.startColor = trail.endColor = new Color(0, 0, 1f, 0.8f);
+                trail.startWidth = trail.endWidth = 1f;
+                break;
+            case ColorType.none:
+            default:
+                break;
+        }
+        while(Input.GetMouseButton(0))
+        {
+            Plane objPlane = new Plane(Camera.main.transform.forward * -1, transform.position);
+            Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float rayDistance;
+            if (objPlane.Raycast(mRay, out rayDistance))
+            {
+                Vector3 pos = mRay.GetPoint(rayDistance);
+                transform.position = pos;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzles/Painter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painter : MonoBehaviour
+{
+    public PaintTrail.ColorType c = PaintTrail.ColorType.none;
+
+    public GameObject trailPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            PaintTrail p = Instantiate(trailPrefab,
+                                       new Vector3(mousePos.x, mousePos.y, 0f),
+                                       Quaternion.identity, transform).GetComponent<PaintTrail>();
+            p.color = c;
+            StartCoroutine(p.Paint());
+        }
+    }
+}
