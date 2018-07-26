6d44e2d3237b2adedc203cbf3535ab7119e80514
Zilby <azilby@gmail.com>
2018-07-26 17:54:53 -0400

    Menu stuff

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -9,16 +9,48 @@ using UnityEngine.UI;
 /// </summary>
 public class MenuManager : MonoBehaviour
 {
-
-    public Button play;
+    public GameObject main;
+    public GameObject levelSelect;
+    public GameObject credits;
+    public GameObject settings;
 
     private void Awake()
     {
-        play.onClick.AddListener(StartGame);
+        Button[] mainButtons = main.GetComponentsInChildren<Button>();
+
+        mainButtons[0].onClick.AddListener(StartGame);
+        mainButtons[1].onClick.AddListener(Continue);
+        mainButtons[2].onClick.AddListener(delegate { InvertPage(levelSelect); });
+        mainButtons[3].onClick.AddListener(delegate { InvertPage(credits); });
+        mainButtons[4].onClick.AddListener(delegate { InvertPage(settings); });
+        mainButtons[5].onClick.AddListener(Exit);
+
+        levelSelect.GetComponentInChildren<Button>().onClick.AddListener(delegate { InvertPage(levelSelect); });
+        credits.GetComponentInChildren<Button>().onClick.AddListener(delegate { InvertPage(credits); });
+        settings.GetComponentInChildren<Button>().onClick.AddListener(delegate { InvertPage(settings); });
     }
 
     private void StartGame()
     {
         SceneManager.LoadScene("Lust");
+    }
+
+    private void Continue()
+    {
+
+    }
+
+    private void InvertPage(GameObject g) {
+        g.SetActive(!g.activeSelf);
+        main.SetActive(!main.activeSelf);
+    }
+
+    private void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
