86ba8a1381f8f213e5baab26900d18e35c95875f
Zilby <azilby@gmail.com>
2018-08-26 12:39:49 -0400

    Added repeatable background

---
+++ Assets/Scripts/Game/RepeatableBackground.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RepeatableBackground : MonoBehaviour
+{
+#if UNITY_EDITOR
+	public Sprite s;
+
+	[Range(0,10)]
+	public int backgrounds;
+
+	public Vector2 size;
+
+	private int oldBackgrounds;
+	private Vector2 oldSize;
+
+	void Reset()
+	{
+		size = Vector2.one;
+		oldSize = Vector2.one;
+		oldBackgrounds = 0;
+	}
+
+	void Update()
+	{
+		if (oldBackgrounds != backgrounds)
+		{
+			int i = 0;
+			while (i < transform.childCount)
+			{
+				DestroyImmediate(transform.GetChild(i).gameObject);
+			}
+			for (; i < backgrounds; ++i)
+			{
+				GameObject g = new GameObject("Bg " + i);
+				g.transform.parent = transform;
+				SpriteRenderer sr = g.AddComponent<SpriteRenderer>();
+				sr.sprite = s;
+				SpriteUtility.ResizeSpriteRendererToDimensions(size, sr);
+			}
+			Reposition();
+			oldBackgrounds = backgrounds;
+		}
+		if (oldSize != size)
+		{
+			foreach (Transform child in transform)
+			{
+				SpriteUtility.ResizeSpriteRendererToDimensions(size, child.GetComponent<SpriteRenderer>());
+			}
+			Reposition();
+			oldSize = size;
+		}
+	}
+
+	private void Reposition()
+	{
+		float x = (size.x / -2f) * (backgrounds - 1);
+		foreach (Transform child in transform)
+		{
+			child.localPosition = new Vector3(x, 0, 0);
+			x += size.x;
+		}
+	}
+#endif
+}
---
+++ Assets/Scripts/Utility/SpriteUtility.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Unity sprite utility class for helping utilizing sprites. 
+/// </summary>
+public static class SpriteUtility
+{
+
+	/// <summary>
+	/// Resizes the given sprite renderer's local scale to the given dimensions. 
+	/// </summary>
+	public static void ResizeSpriteRendererToDimensions(Vector2 dimensions, SpriteRenderer s)
+	{
+		Vector3 spriteBounds = s.bounds.size;
+		if (spriteBounds != Vector3.zero && dimensions.x > 0 && dimensions.y > 0)
+		{
+			s.transform.localScale = new Vector3(s.transform.localScale.x * dimensions.x / spriteBounds.x,
+												 s.transform.localScale.y * dimensions.y / spriteBounds.y, 1);
+		}
+	}
+
+	/// <summary>
+	/// Resizes the given sprite renderer's local scale to the given rect transform's size delta dimensions. 
+	/// </summary>
+	public static void ResizeSpriteRendererToRect(RectTransform rect, SpriteRenderer s)
+	{
+		Vector2 dimensions = rect.sizeDelta;
+		ResizeSpriteRendererToDimensions(dimensions, s);
+	}
+
+	/// <summary>
+	/// Resizes the given sprite renderer's local scale to the given gameobject's rect transform's size delta dimensions. 
+	/// </summary>
+	public static void ResizeSpriteRendererToGObj(GameObject g, SpriteRenderer s)
+	{
+		RectTransform rect = g.GetComponent<RectTransform>();
+		ResizeSpriteRendererToRect(rect, s);
+	}
+}
