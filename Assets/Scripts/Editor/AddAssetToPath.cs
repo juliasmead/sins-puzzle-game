a959ac91ee0cb171f9add11477a5ff7b36cb2a6a
Zilby <azilby@gmail.com>
2018-08-25 20:02:34 -0400

    What am I doing with my life

--- Assets/Scripts/Editor/AddAssetToPath.cs
+++ Assets/Scripts/Editor/AddAssetToPath.cs
@@ -2,34 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Animations;
 
 /// <summary>
-/// Editor tool for adding assets to a given object. 
+/// Editor tool for adding animation to an animator
 /// </summary>
-public class AddAssetToPath : EditorWindow
+public class AddAnimationToAnimator : EditorWindow
 {
-	public Object assetToAdd;
-	public Object assetAddedTo;
+	public AnimationClip animation;
+	public AnimatorController animator;
 
-	[MenuItem("Assets/Add Asset To Path")]
+	[MenuItem("Assets/Add Animation to Animator")]
 	static void Init()
 	{
-		EditorWindow window = GetWindow(typeof(AddAssetToPath));
+		EditorWindow window = GetWindow(typeof(AddAnimationToAnimator));
 		window.Show();
 	}
 
 	void OnGUI()
 	{
 		EditorGUILayout.Space();
-		assetToAdd = EditorGUILayout.ObjectField("Asset to add", assetToAdd, typeof(Object), true);
-		assetAddedTo = EditorGUILayout.ObjectField("Asset added to", assetAddedTo, typeof(Object), true);
+		animation = (AnimationClip)EditorGUILayout.ObjectField("Animation", animation, typeof(AnimationClip), true);
+		animator = (AnimatorController)EditorGUILayout.ObjectField("Animator", animator, typeof(AnimatorController), true);
 		EditorGUILayout.Space();
 		if (GUILayout.Button("Add Asset to path"))
 		{
-			if (assetToAdd != null && assetAddedTo != null)
+			if (animation != null && animator != null)
 			{
-				AssetDatabase.AddObjectToAsset(assetToAdd, assetAddedTo);
-				AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(assetAddedTo));
+				AnimationClip a = Instantiate(animation);
+				string n = animation.name;
+				AssetDatabase.DeleteAsset(AssetDatabase.GetAssetPath(animation));
+				a.name = n;
+				AssetDatabase.AddObjectToAsset(a, animator);
+				AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(animator));
 				Debug.Log("Successfully added asset to path.");
 			}
 			else
